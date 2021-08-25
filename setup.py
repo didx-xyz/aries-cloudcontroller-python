c47742d36ccb6f7f3705d1daee33e4badf55b4aa
Timo Glastra <timo@animo.id>
2021-08-25 09:15:41 +0200

    add py.typed for better type hinting
    Signed-off-by: Timo Glastra <timo@animo.id>

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 """Module setup."""
 
 import os
-import runpy
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = "aries_cloudcontroller"
-# version_meta = runpy.run_path("./{}/version.py".format(PACKAGE_NAME))
-# VERSION = version_meta["__version__"]
-
 
 with open(os.path.abspath("README.md"), "r") as fh:
     long_description = fh.read()
 
 
-def parse_requirements(filename):
+def parse_requirements(filename: str):
     """Load requirements from a pip requirements file."""
     lineiter = (line.strip() for line in open(filename))
     return [line for line in lineiter if line and not line.startswith("#")]
@@ -29,7 +25,12 @@ if __name__ == "__main__":
         url="https://github.com/didx-xyz/aries-cloudcontroller-python/tree/main/aries_cloudcontroller",
         packages=find_packages(),
         include_package_data=True,
-        package_data={"aries_cloudcontroller": ["requirements.txt"]},
+        package_data={
+            "aries_cloudcontroller": [
+                "requirements.txt",
+                "aries_cloudcontroller/py.typed",
+            ]
+        },
         install_requires=parse_requirements("requirements.txt"),
         python_requires=">=3.8",
         classifiers=[
