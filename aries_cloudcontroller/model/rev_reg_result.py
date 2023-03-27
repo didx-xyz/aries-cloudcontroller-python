786ee271c5974103dd8861e4e19ffc4681646c76
lohanspies <lohan.spies@didx.co.za>
2023-03-27 10:03:48 +0200

    fix optional arguments in revocation registry

--- aries_cloudcontroller/model/rev_reg_result.py
+++ aries_cloudcontroller/model/rev_reg_result.py
@@ -17,15 +17,15 @@ class RevRegResult(BaseModel):
     Do not edit the class manually.
 
     RevRegResult - a model defined in OpenAPI
-        result: The result of this RevRegResult [Optional].
+        result: The result of this RevRegResult.
     """
 
-    result: Optional[IssuerRevRegRecord] = None
+    result: IssuerRevRegRecord
 
     def __init__(
         self,
         *,
-        result: Optional[IssuerRevRegRecord] = None,
+        result: IssuerRevRegRecord = None,
         **kwargs,
     ):
         super().__init__(
