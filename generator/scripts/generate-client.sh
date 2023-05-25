07b83ccad115a2d5101903b37dd2ea0ae647924c
ff137 <ff137@proton.me>
2023-05-25 21:58:06 +0200

    :art:

--- generator/scripts/generate-client.sh
+++ generator/scripts/generate-client.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")/../" || exit
 rm -rf ../generated/
 
 # Generated client
-java  -ea -server -Duser.timezone=UTC -jar "$(pwd)/../../openapi-generator/modules/openapi-generator-cli/target/openapi-generator-cli.jar" generate -c  ./openapi-generator-config.yml --skip-validate-spec
+java -ea -server -Duser.timezone=UTC -jar "$(pwd)/../../openapi-generator/modules/openapi-generator-cli/target/openapi-generator-cli.jar" generate -c ./openapi-generator-config.yml --skip-validate-spec
 
 # Copy 
 cp -r ../generated/aries_cloudcontroller/ ../aries_cloudcontroller/
