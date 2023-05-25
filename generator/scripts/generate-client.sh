4bc94e4fe2b14dbddc03d204f3f345f9abaa16ed
ff137 <ff137@proton.me>
2023-05-25 21:58:33 +0200

    adjust copy path

--- generator/scripts/generate-client.sh
+++ generator/scripts/generate-client.sh
@@ -9,8 +9,8 @@ rm -rf ../generated/
 # Generated client
 java -ea -server -Duser.timezone=UTC -jar "$(pwd)/../../openapi-generator/modules/openapi-generator-cli/target/openapi-generator-cli.jar" generate -c ./openapi-generator-config.yml --skip-validate-spec
 
-# Copy 
-cp -r ../generated/aries_cloudcontroller/ ../aries_cloudcontroller/
+# Copy
+cp -r ../generated/aries_cloudcontroller/ ..
 
 # Apply the patches required
 cd ..
