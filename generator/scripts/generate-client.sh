10545738712684e2d1a87ef4a07cf43586eee537
ff137 <ff137@proton.me>
2023-10-10 14:06:28 +0200

    :art: add isort step to generated output, and comment out patching

--- generator/scripts/generate-client.sh
+++ generator/scripts/generate-client.sh
@@ -13,14 +13,16 @@ java -ea -server -Duser.timezone=UTC -jar "$(pwd)/../../openapi-generator/module
 cp -r ../generated/aries_cloudcontroller/ ..
 
 black .
+isort . --profile black
 
 # Apply the patches required
-cd ..
+# cd ..
 
-echo -e "\nApplying patch"
-git apply --verbose generator/data/__init__.patch || {
-    # git apply failed! Warn the user
-    echo -e "$(tput setaf 1)\n\nFailed to apply patch. Client generation INCOMPLETE!\n\nPlease, ensure to fix this before proceeding.\nTake care when committing unpatched code.\n\n"
-    exit 1
-}
-black .
+# echo -e "\nApplying patch"
+# git apply --verbose generator/data/__init__.patch || {
+#     # git apply failed! Warn the user
+#     echo -e "$(tput setaf 1)\n\nFailed to apply patch. Client generation INCOMPLETE!\n\nPlease, ensure to fix this before proceeding.\nTake care when committing unpatched code.\n\n"
+#     exit 1
+# }
+# black .
+# isort . --profile black
