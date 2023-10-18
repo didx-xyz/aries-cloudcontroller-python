779574e0b6eea89b1f81e7afd83b20129c9341a6
ff137 <ff137@proton.me>
2023-10-18 16:27:06 +0200

    :art: set `validate_assignment` to False

--- aries_cloudcontroller/util/pydantic_model_config.py
+++ aries_cloudcontroller/util/pydantic_model_config.py
@@ -3,5 +3,5 @@ from pydantic import ConfigDict
 DEFAULT_PYDANTIC_MODEL_CONFIG: ConfigDict = {
     "defer_build": True,
     "populate_by_name": True,
-    "validate_assignment": True,
+    "validate_assignment": False,
 }
