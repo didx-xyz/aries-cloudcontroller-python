5fd7dabf4d54f0287a063bbed31014e1fa6f9cb5
ff137 <ff137@proton.me>
2023-10-20 14:07:23 +0200

    :art::sparkles: additional postprocessing to modify faults in autogeneration

--- generator/scripts/postprocess-client.sh
+++ generator/scripts/postprocess-client.sh
@@ -40,7 +40,30 @@ sed -i -E 's/from aries_cloudcontroller\.(\w+)\.\w+ /from aries_cloudcontroller.
     echo "from aries_cloudcontroller.util import DEFAULT_PYDANTIC_MODEL_CONFIG"
 } >>aries_cloudcontroller/__init__.py
 
-# Fix timestamp
+# Fix var_json field in attach_decorator_data.py (Union[str, Any] -> Union[Dict[str, Any], List[Dict[str, Any]]])
+sed -i 's/var_json: Optional\[Union\[str, Any\]\] = Field(/var_json: Optional\[Union\[Dict\[str, Any\], List\[Dict\[str, Any\]\]\]\] = Field(/g' aries_cloudcontroller/models/attach_decorator_data.py
+
+# Fix types in filter.py model to accommodate custom schema in ACA-Py (replace `Optional[Union[str, Any]]` with `Optional[Union[str, int, float]]`
+sed -i 's/\[Union\[str, Any\]/\[Union\[str, int, float\]/g' aries_cloudcontroller/models/filter.py
+
+# Fix types in credential.py: "Any" type in context and issuer fields, should be a dict
+sed -i -e 's/context: List\[Union\[str, Any\]\]/context: List\[Union\[str, Dict\]\]/g' \
+    -e 's/issuer: Union\[str, Any\]/issuer: Union\[str, Dict\[str, Any\]\]/g' \
+    aries_cloudcontroller/models/credential.py
+
+# Fix type in invitation_message.py: "Any" type in services should be a dict
+sed -i 's/services: Optional\[List\[Union\[str, Any\]\]\]/services: Optional\[List\[Union\[str, Dict\]\]\]/g' aries_cloudcontroller/models/invitation_message.py
+
+# NB:
+# There are 3 more models, and 1 API Module, that we are not amending automatically. These should be reviewed manually:
+# - MultitenancyAPI has custom method to handle our groups plugin!
+# Models:
+# - credential_definition.py: `type`, for now, must be Literal["CL"]
+# - did.py: key_type, method, and posture each have literals. NB: Also: the validator: `verkey_validate_regular_expression` must include BBS_PATTERN regex
+# - verify_request: doc: SignedDoc must be relaxed to: doc: Dict[str, Any], and `if self.doc:` override block must be removed
+
+# Additionally, the API Client we modify so that query_params are converted from bool to str, before being submitted to ACA-Py
+# This change impacts multiple lines, calling `sanitize_for_serialization`
 
 # Black format and optimise imports
 black aries_cloudcontroller
