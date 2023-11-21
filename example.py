ade04c370ca20a7bdc6721475c2d1dddfe9741b0
ff137 <ff137@proton.me>
2023-11-21 17:16:10 +0200

    :art: Update example

--- example.py
+++ example.py
@@ -1,16 +1,30 @@
 import asyncio
 
-from aries_cloudcontroller import AcaPyClient, CreateInvitationRequest
+from aries_cloudcontroller import (
+    AcaPyClient,
+    CreateInvitationRequest,
+    InvitationResult,
+    ReceiveInvitationRequest,
+    ConnRecord,
+)
 
 client = AcaPyClient(base_url="https://agent.community.animo.id", admin_insecure=True)
 
 
 async def run():
-    result = await client.connection.create_invitation(
+    result: InvitationResult = await client.connection.create_invitation(
         body=CreateInvitationRequest(my_label="Cloud Controller")
     )
 
-    connection = await client.connection.receive_invitation(body=result.invitation)
+    # Pydantic v2 introduced strict model typing, so the InvitationResult must be converted to
+    # a ReceiveInvitationRequest. These models share identical fields, and so can be converted:
+    receive_invitation_request = ReceiveInvitationRequest.from_dict(
+        result.invitation.to_dict()
+    )
+
+    connection: ConnRecord = await client.connection.receive_invitation(
+        body=receive_invitation_request
+    )
 
     print(connection)
 
