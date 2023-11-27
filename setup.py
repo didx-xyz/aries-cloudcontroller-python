85d14537ec47cf46a1aac195e90dbf1ddd94f6cb
ff137 <ff137@proton.me>
2023-11-27 12:18:50 +0200

    v0.10.5

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def parse_requirements(filename: str):
 if __name__ == "__main__":
     setup(
         name=PACKAGE_NAME,
-        version="0.10.4",
+        version="0.10.5",
         description="A simple python client for controlling an ACA-Py agent",
         long_description=long_description,
         long_description_content_type="text/markdown",
