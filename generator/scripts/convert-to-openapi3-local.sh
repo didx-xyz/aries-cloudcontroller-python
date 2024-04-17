5c152a489d800c10e0dc5f970d6dc53a34a350b7
ff137 <ff137@proton.me>
2024-04-17 15:48:23 +0200

    :arrow_up: upgrade swagger-converter

--- generator/scripts/convert-to-openapi3-local.sh
+++ generator/scripts/convert-to-openapi3-local.sh
@@ -5,7 +5,7 @@ CONTAINER_RUNTIME=${CONTAINER_RUNTIME:-docker}
 
 CONTAINER_NAME=openapi-converter
 
-${CONTAINER_RUNTIME} run --rm -d -p 8080:8080 --name ${CONTAINER_NAME} swaggerapi/swagger-converter:v1.0.2
+${CONTAINER_RUNTIME} run --rm -d -p 8080:8080 --name ${CONTAINER_NAME} swaggerapi/swagger-converter:v1.0.5
 trap '${CONTAINER_RUNTIME} stop ${CONTAINER_NAME}' EXIT
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080)" != "200" ]];do
     echo "Converter not yet ready..."
