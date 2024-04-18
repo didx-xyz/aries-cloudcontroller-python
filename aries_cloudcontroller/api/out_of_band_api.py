f38f6cabdddc2e0aa55b02afb837c676d7b55361
ff137 <ff137@proton.me>
2024-04-18 15:42:01 +0200

    :sparkles: new method for OutOfBandApi: Delete records associated with invitation

--- aries_cloudcontroller/api/out_of_band_api.py
+++ aries_cloudcontroller/api/out_of_band_api.py
@@ -305,6 +305,189 @@ class OutOfBandApi:
         )
 
     @validate_call
+    async def out_of_band_invitations_invi_msg_id_delete(
+        self,
+        invi_msg_id: Annotated[
+            str, Field(strict=True, description="Invitation Message identifier")
+        ],
+        _request_timeout: Union[
+            None,
+            Annotated[StrictFloat, Field(gt=0)],
+            Tuple[
+                Annotated[StrictFloat, Field(gt=0)], Annotated[StrictFloat, Field(gt=0)]
+            ],
+        ] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> object:
+        """Delete records associated with invitation
+
+
+        :param invi_msg_id: Invitation Message identifier (required)
+        :type invi_msg_id: str
+        ...
+        """  # noqa: E501
+
+        _param = self._out_of_band_invitations_invi_msg_id_delete_serialize(
+            invi_msg_id=invi_msg_id,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index,
+        )
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            "200": "object",
+        }
+        response_data = await self.api_client.call_api(
+            *_param, _request_timeout=_request_timeout
+        )
+        await response_data.read()
+        return self.api_client.response_deserialize(
+            response_data=response_data,
+            response_types_map=_response_types_map,
+        ).data
+
+    async def out_of_band_invitations_invi_msg_id_delete_with_http_info(
+        self,
+        invi_msg_id: Annotated[
+            str, Field(strict=True, description="Invitation Message identifier")
+        ],
+        _request_timeout: Union[
+            None,
+            Annotated[StrictFloat, Field(gt=0)],
+            Tuple[
+                Annotated[StrictFloat, Field(gt=0)], Annotated[StrictFloat, Field(gt=0)]
+            ],
+        ] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> ApiResponse[object]:
+        """Delete records associated with invitation
+
+
+        :param invi_msg_id: Invitation Message identifier (required)
+        :type invi_msg_id: str
+        ...
+        """  # noqa: E501
+
+        _param = self._out_of_band_invitations_invi_msg_id_delete_serialize(
+            invi_msg_id=invi_msg_id,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index,
+        )
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            "200": "object",
+        }
+        response_data = await self.api_client.call_api(
+            *_param, _request_timeout=_request_timeout
+        )
+        await response_data.read()
+        return self.api_client.response_deserialize(
+            response_data=response_data,
+            response_types_map=_response_types_map,
+        )
+
+    async def out_of_band_invitations_invi_msg_id_delete_without_preload_content(
+        self,
+        invi_msg_id: Annotated[
+            str, Field(strict=True, description="Invitation Message identifier")
+        ],
+        _request_timeout: Union[
+            None,
+            Annotated[StrictFloat, Field(gt=0)],
+            Tuple[
+                Annotated[StrictFloat, Field(gt=0)], Annotated[StrictFloat, Field(gt=0)]
+            ],
+        ] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> RESTResponseType:
+        """Delete records associated with invitation
+
+
+        :param invi_msg_id: Invitation Message identifier (required)
+        :type invi_msg_id: str
+        ...
+        """  # noqa: E501
+
+        _param = self._out_of_band_invitations_invi_msg_id_delete_serialize(
+            invi_msg_id=invi_msg_id,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index,
+        )
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            "200": "object",
+        }
+        response_data = await self.api_client.call_api(
+            *_param, _request_timeout=_request_timeout
+        )
+        return response_data.response
+
+    def _out_of_band_invitations_invi_msg_id_delete_serialize(
+        self,
+        invi_msg_id,
+        _request_auth,
+        _content_type,
+        _headers,
+        _host_index,
+    ) -> RequestSerialized:
+
+        _host = None
+
+        _collection_formats: Dict[str, str] = {}
+
+        _path_params: Dict[str, str] = {}
+        _query_params: List[Tuple[str, str]] = []
+        _header_params: Dict[str, Optional[str]] = _headers or {}
+        _form_params: List[Tuple[str, str]] = []
+        _files: Dict[str, Union[str, bytes]] = {}
+        _body_params: Optional[bytes] = None
+
+        # process the path parameters
+        if invi_msg_id is not None:
+            _path_params["invi_msg_id"] = invi_msg_id
+        # process the query parameters
+        # process the header parameters
+        # process the form parameters
+        # process the body parameter
+
+        # set the HTTP header `Accept`
+        _header_params["Accept"] = self.api_client.select_header_accept(
+            ["application/json"]
+        )
+
+        # authentication setting
+        _auth_settings: List[str] = ["AuthorizationHeader"]
+
+        return self.api_client.param_serialize(
+            method="DELETE",
+            resource_path="/out-of-band/invitations/{invi_msg_id}",
+            path_params=_path_params,
+            query_params=_query_params,
+            header_params=_header_params,
+            body=_body_params,
+            post_params=_form_params,
+            files=_files,
+            auth_settings=_auth_settings,
+            collection_formats=_collection_formats,
+            _host=_host,
+            _request_auth=_request_auth,
+        )
+
+    @validate_call
     async def receive_invitation(
         self,
         alias: Annotated[
