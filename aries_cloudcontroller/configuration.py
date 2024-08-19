c38c39492be3cf59c95f80a003f5b2ef48879ba5
cl0ete <cloete.dupreez@gmail.com>
2024-08-19 11:07:22 +0200

    :art:

--- aries_cloudcontroller/configuration.py
+++ aries_cloudcontroller/configuration.py
@@ -425,9 +425,7 @@ class Configuration:
             "OS: {env}\n"
             "Python Version: {pyversion}\n"
             "Version of the API: v1.0.0\n"
-            "SDK Package Version: 1.0.0".format(
-                env=sys.platform, pyversion=sys.version
-            )
+            "SDK Package Version: 1.0.0".format(env=sys.platform, pyversion=sys.version)
         )
 
     def get_host_settings(self):
