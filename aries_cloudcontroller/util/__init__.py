e0d3616d7a0ab7983b3e1c6456b1f586e2b06225
ff137 <ff137@proton.me>
2024-08-29 14:07:28 +0300

    :art:

--- aries_cloudcontroller/util/__init__.py
+++ aries_cloudcontroller/util/__init__.py
@@ -1,8 +1,6 @@
 from aries_cloudcontroller.util.pydantic_model_config import (
     DEFAULT_PYDANTIC_MODEL_CONFIG as DEFAULT_PYDANTIC_MODEL_CONFIG,
 )
-from aries_cloudcontroller.util.regex_patterns import (
-    BASE58_PATTERN as BASE58_PATTERN,
-    BBS_PATTERN as BBS_PATTERN,
-    ED25519_PATTERN as ED25519_PATTERN,
-)
+from aries_cloudcontroller.util.regex_patterns import BASE58_PATTERN as BASE58_PATTERN
+from aries_cloudcontroller.util.regex_patterns import BBS_PATTERN as BBS_PATTERN
+from aries_cloudcontroller.util.regex_patterns import ED25519_PATTERN as ED25519_PATTERN
