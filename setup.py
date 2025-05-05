7d8362687ee7e4966583bf47fa641a0640e6bf14
ff137 <ff137@proton.me>
2025-05-05 14:42:27 +0300

    :boomark: v1.3.0.post20250505

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def parse_requirements(filename: str):
 if __name__ == "__main__":
     setup(
         name=PACKAGE_NAME,
-        version="1.3.0rc1-20250417",
+        version="1.3.0-20250505",
         description="A simple python client for controlling an ACA-Py agent",
         long_description=long_description,
         long_description_content_type="text/markdown",
